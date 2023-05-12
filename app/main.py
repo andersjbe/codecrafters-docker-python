603b9758c762abc5f3ad950a68aae3c962d23e2a
Ben Anderson <andersjbe@gmail.com>
2023-05-12 10:09:44 -0700

    Added exit code

--- app/main.py
+++ app/main.py
@@ -6,13 +6,14 @@ def main():
     command = sys.argv[3]
     args = sys.argv[4:]
 
-    if args[0] == 'echo':
-        print(args[1])
-    elif args[0] == 'echo_stderr':
-        print(args[1], file=sys.stderr)
+    completed_process = subprocess.run([command, *args], capture_output=True, )
 
-    # completed_process = subprocess.run([command, *args], capture_output=True)
-    # print(completed_process.stdout.decode("utf-8"))
+    if args[0] == 'echo':
+        print(completed_process.stdout.decode("utf-8"))
+    elif args[0] == 'echo_stderr':
+        print(completed_process.stdout.decode("utf-8"), file=sys.stderr)
+
+    quit(completed_process.returncode)
 
 
 if __name__ == "__main__":
